a542b216229e5820c470f6490e70a764e836544c
zhaojingbin <793808306@qq.com>
2020-07-22 11:01:31 +0800

    更改数据库配置

--- config/config.default.js
+++ config/config.default.js
@@ -6,118 +6,118 @@
  * @param {Egg.EggAppInfo} appInfo app info
  */
 module.exports = appInfo => {
-  /**
-   * built-in config
-   * @type {Egg.EggAppConfig}
-   **/
-  const config = exports = {};
+    /**
+     * built-in config
+     * @type {Egg.EggAppConfig}
+     **/
+    const config = exports = {};
 
-  // use for cookie sign key, should change to your own and keep security
-  config.keys = appInfo.name + '_1559271081074_4657';
+    // use for cookie sign key, should change to your own and keep security
+    config.keys = appInfo.name + '_1559271081074_4657';
 
-  // exports.cluster = {
-  //   listen: {
-  //     port: 6666,
-  //     hostname: '192.168.164.97',
-  //     // path: '/var/run/egg.sock',
-  //   }
-  // }
+    // exports.cluster = {
+    //   listen: {
+    //     port: 6666,
+    //     hostname: '192.168.164.97',
+    //     // path: '/var/run/egg.sock',
+    //   }
+    // }
 
-  config.url = '127.0.0.1:7001';
-  // add your middleware config here
-  config.middleware = [];
+    config.url = '127.0.0.1:7001';
+    // add your middleware config here
+    config.middleware = [];
 
-  // add your user config here
-  const userConfig = {
-    // myAppName: 'egg',
-    security: {
-      csrf: {
-        enable: false
-      },
-      domainWhiteList: ['*']
-    },
-    cors: {
-      allowMethods: 'GET,POST,HEAD,PUT,DELETE,PATCH,OPTIONS'
-    },
-    multipart: {
-      fileSize: '1500mb',
-      fileExtensions: [
-        '.foo',
-        '.apk',
-        '.rar',
-        '.zip',
-        '.js',
-        '.html',
-        '.css',
-        '.doc',
-        '.docx',
-        '.apk',
-        '.ipa'
-      ]
-    },
-    bodyParser: {
-      jsonLimit: '5mb',
-      formLimit: '5mb'
-    },
-    sequelize: {
-      dialect: 'mysql',
-      // host: '172.17.0.4',
-      // host: '192.168.31.161',
-      // username: 'root',
-      host: '45.249.247.221', // 线上服务器
-      username: 'root', // 线上服务器
-      port: 3306,
-      database: 'shops',
-      password: 'a123456'
-    },
-    swagger2: {
-      enable: true,
-      base: {
-        info: {
-          description: 'this is a test swagger-ui html',
-          version: '1.0.0',
-          title: 'test',
-          contact: {
-            email: 'syfubest@gmail.com',
-          },
-          license: {
-            name: 'Apache 2.0',
-            url: 'http://www.apache.org/license/LICENSE-2.0.html'
-          },
+    // add your user config here
+    const userConfig = {
+        // myAppName: 'egg',
+        security: {
+            csrf: {
+                enable: false
+            },
+            domainWhiteList: ['*']
         },
-        tags: [{
-          name: 'auth',
-          description: '登录注册'
-        },{
-          name: 'app',
-          description: 'app相关'
-        },{
-          name: 'admin',
-          description: 'Admin desc'
-        },{
-          name: 'role',
-          description: 'Role desc'
-        }],
-        definitions: {
-
+        cors: {
+            allowMethods: 'GET,POST,HEAD,PUT,DELETE,PATCH,OPTIONS'
         },
-        securityDefinitions: {
-          // basicAuth: {
-          //   type: 'basic',
-          //   // scheme: 'bearer'
-          // }
-          Bearer: {
-            type: 'apiKey',
-            name: 'Authorization',
-            in: 'header'
-          }
+        multipart: {
+            fileSize: '1500mb',
+            fileExtensions: [
+                '.foo',
+                '.apk',
+                '.rar',
+                '.zip',
+                '.js',
+                '.html',
+                '.css',
+                '.doc',
+                '.docx',
+                '.apk',
+                '.ipa'
+            ]
+        },
+        bodyParser: {
+            jsonLimit: '5mb',
+            formLimit: '5mb'
+        },
+        sequelize: {
+            dialect: 'mysql',
+            // host: '172.17.0.4',
+            // host: '192.168.31.161',
+            // username: 'root',
+            host: '129.211.59.88', // 线上服务器
+            username: 'root', // 线上服务器
+            port: 3306,
+            database: 'supermall',
+            password: 'Zhaojingbin'
+        },
+        swagger2: {
+            enable: true,
+            base: {
+                info: {
+                    description: 'this is a test swagger-ui html',
+                    version: '1.0.0',
+                    title: 'test',
+                    contact: {
+                        email: 'syfubest@gmail.com',
+                    },
+                    license: {
+                        name: 'Apache 2.0',
+                        url: 'http://www.apache.org/license/LICENSE-2.0.html'
+                    },
+                },
+                tags: [{
+                    name: 'auth',
+                    description: '登录注册'
+                }, {
+                    name: 'app',
+                    description: 'app相关'
+                }, {
+                    name: 'admin',
+                    description: 'Admin desc'
+                }, {
+                    name: 'role',
+                    description: 'Role desc'
+                }],
+                definitions: {
+
+                },
+                securityDefinitions: {
+                    // basicAuth: {
+                    //   type: 'basic',
+                    //   // scheme: 'bearer'
+                    // }
+                    Bearer: {
+                        type: 'apiKey',
+                        name: 'Authorization',
+                        in: 'header'
+                    }
+                }
+            }
         }
-      }
-    }
-  };
+    };
 
-  return {
-    ...config,
-    ...userConfig,
-  };
+    return {
+        ...config,
+        ...userConfig,
+    };
 };
